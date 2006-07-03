4ef4f32b135c90473eeeaa14c69ad56d6b378c61
kold <kold@388701f2-3217-0410-a608-c2b1fb36d0f3>
2006-07-03 15:03:44 +0000

    added regex explain to help test regexes
    git-svn-id: svn+ssh://kold.homelinux.com/home/kold/authorwatch@24 388701f2-3217-0410-a608-c2b1fb36d0f3

---
+++ scrape/regex_explain.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+#Written by Kevin at Wed 29 Mar 2006 01:35:14 PM EST
+
+use YAPE::Regex::Explain;
+
+
+#my $reg = /^IMAGES\:EPS_files\:(.*)$/;
+my $reg = '(?-imsx:/^(?:IMAGES:EPS_files)?:?(.*)/)';
+my $exp = YAPE::Regex::Explain->new($reg)->explain;
+
+print $exp;
