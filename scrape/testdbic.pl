a24164cfc4e147ee801ba2dd0511936151c6f984
kold <kold@388701f2-3217-0410-a608-c2b1fb36d0f3>
2006-07-03 20:21:03 +0000

    checking in changes to scripts
    git-svn-id: svn+ssh://kold.homelinux.com/home/kold/authorwatch@28 388701f2-3217-0410-a608-c2b1fb36d0f3

---
+++ scrape/testdbic.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+#Written by Kevin at Mon 03 Jul 2006 03:53:37 PM EDT
+use lib ('/home/kevin/projects/authorwatch/lib');
+use AwDB;
+
+my $schema = AwDB->connect('dbi:mysql:database=authorwatch;host=kold.homelinux.com;port=3306','root','kevin');
+
+$schema->resultset('Authors')->create({ first_name => 'John', last_name => 'Grisham' });
+
+my @all_authors = $schema->resultset('Authors')->all;
+
+foreach my $auth ( @all_authors ) {
+
+    print $auth->first_name, $auth->last_name, "\n";
+
+}
