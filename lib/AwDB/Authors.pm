c454a32a0311b61489da4337b76d7c9a5c12c3ec
kold <kold@388701f2-3217-0410-a608-c2b1fb36d0f3>
2006-08-08 11:50:26 +0000

    added user authors lookup, but have problem with getting user/list_authors to return data
    git-svn-id: svn+ssh://kold.homelinux.com/home/kold/authorwatch@47 388701f2-3217-0410-a608-c2b1fb36d0f3

--- lib/AwDB/Authors.pm
+++ lib/AwDB/Authors.pm
@@ -28,7 +28,7 @@ __PACKAGE__->set_primary_key('id');
 #     1) Name of relationship, DBIC will create accessor with this name
 #     2) Name of the model class referenced by this relationship
 #     3) Column name in *foreign* table
-__PACKAGE__->has_many(user_messages => 'AwDB::UserMessages', 'message_id');
+__PACKAGE__->has_many(user_authors => 'AwDB::UserAuthors', 'user_id');
 __PACKAGE__->has_many(map_user_role => 'AwDB::UserRoles', 'user_id');
 
 # many_to_many():
--- lib/authorwatch/C/User.pm
+++ lib/authorwatch/C/User.pm
@@ -42,6 +42,8 @@ sub save_author : Local {
 }
 
 
+
+
 =head2 list_authors
 
 Show a users list of authors
@@ -51,11 +53,37 @@ Show a users list of authors
 sub list_authors : Local {
     my ( $self, $c ) = @_;
 
+    # Debug sql statements
+    $c->model('AwDB::UserAuthors')->storage->debug(1);
+
     my $user_id = $c->req->param("user_id");
 
     # Lookup authors for User
+    my @authors;
+    @authors = $c->model('AwDB::UserAuthors')->search({ user_id => $user_id });
+=pod
+        {   -or => {
+                first_name => { -like => "%$term%" },
+                last_name  => { -like => "%$term%" }
+            }
+        }
+=cut
 
-    $c->stash->{template} = 'list_authors.mhtml';
+    my @elements;
+    # if returned, parse through them and build html
+    if ( @authors ) {
+
+        foreach my $auth ( @authors ) {
+            my $name = $auth->first_name . ' ' . $auth->last_name;
+            push @elements, HTML::Element->new('li')->push_content($name);    
+        }
+
+        $c->res->body( HTML::Element->new('ul')->push_content(@elements)->as_HTML );
+    } else {
+        $c->response->body("<ul></ul>");
+    }
+
+    #$c->stash->{template} = 'list_authors.mhtml';
 }
 
 =head2 end
