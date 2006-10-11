1711da11ed5f57f95efab4d634efaea58a75b2f5
kold <kold@388701f2-3217-0410-a608-c2b1fb36d0f3>
2006-10-11 02:55:57 +0000

    cleaned code with perltidy
    git-svn-id: svn+ssh://kold.homelinux.com/home/kold/authorwatch@71 388701f2-3217-0410-a608-c2b1fb36d0f3

--- lib/authorwatch/C/Search.pm
+++ lib/authorwatch/C/Search.pm
@@ -35,16 +35,16 @@ Catalyst Controller.
 #sub default : Private {
 #    my ( $self, $c ) = @_;
 
-    # Hello World
-    #$c->response->body( $c->welcome_message );
-    #$c->response->redirect("index.html");
+# Hello World
+#$c->response->body( $c->welcome_message );
+#$c->response->redirect("index.html");
 #}
 
 sub aws : Local {
     my ( $self, $c ) = @_;
 
     $c->stash->{template} = 'results.mhtml';
-    my $svalue = $c->req->param("autocomplete_parameter");
+    my $svalue      = $c->req->param("autocomplete_parameter");
     my $search_term = $c->req->param("autocomplete_parameter");
 
     #$c->log->debug("**********svalue is: $svalue");
@@ -56,8 +56,10 @@ sub aws : Local {
         my $keywords = join " ", @search;
 
         my $today = strftime "%m-%Y", localtime;
-        my $pw_search = uri_escape("author: $keywords and pubdate: after $today and binding: Hardcover");
-        
+        my $pw_search = uri_escape(
+            "author: $keywords and pubdate: after $today and binding: Hardcover"
+        );
+
         # Perform search
         my $response = query_aws($pw_search);
 
@@ -73,7 +75,7 @@ sub aws : Local {
             my $em = $xp->findvalue("//Error/Message");
 
             #$c->stash->{error_msg} = "$ec: $em" ;
-            $c->stash->{error_msg} = "No Results Found." ;
+            $c->stash->{error_msg} = "No Results Found.";
 
             #Put "Author Suggest" code here
         }
@@ -97,43 +99,69 @@ sub aws : Local {
                         );
                 }
 
-                #Check pubdate and make sure year is a valid year and is not too far in the future
-                my $pd = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/ItemAttributes/PublicationDate");
-                my ($yr, $mon, $day) = split /-/, $pd;
-                next unless $yr =~ /^200/;
+#Check pubdate and make sure year is a valid year and is not too far in the future
+                my $pd = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/ItemAttributes/PublicationDate"
+                );
+                my ( $yr, $mon, $day ) = split /-/, $pd;
+                warn "yr is: ", $yr;
+                next unless $yr  =~ /^200/;
                 next unless $mon =~ /^[0|1][0-9]/;
                 next unless $day =~ /^[0|1|2|3][0-9]/;
 
-                my $asin = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/ASIN");
+                my $asin = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/ASIN");
 
-                $records{$asin}{detailpageurl} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/DetailPageURL");
+                $records{$asin}{detailpageurl} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/DetailPageURL");
 
-                $records{$asin}{smallimage} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/SmallImage/URL");
-                $records{$asin}{smallimageheight} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/SmallImage/Height");
-                $records{$asin}{smallimagewidth} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/SmallImage/Width");
+                $records{$asin}{smallimage} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/SmallImage/URL");
+                $records{$asin}{smallimageheight} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/SmallImage/Height");
+                $records{$asin}{smallimagewidth} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/SmallImage/Width");
 
-                $records{$asin}{mediumimage} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/MediumImage/URL");
-                $records{$asin}{mediumimageheight} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/MediumImage/Height");
-                $records{$asin}{mediumimagewidth} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/MediumImage/Width");
+                $records{$asin}{mediumimage} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/MediumImage/URL");
+                $records{$asin}{mediumimageheight} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/MediumImage/Height");
+                $records{$asin}{mediumimagewidth} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/MediumImage/Width");
 
-                $records{$asin}{largeimage} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/LargeImage/URL");
-                $records{$asin}{largeimageheight} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/LargeImage/Height");
-                $records{$asin}{largeimagewidth} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/LargeImage/Width");
+                $records{$asin}{largeimage} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/LargeImage/URL");
+                $records{$asin}{largeimageheight} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/LargeImage/Height");
+                $records{$asin}{largeimagewidth} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/LargeImage/Width");
 
-                $records{$asin}{editorialreview} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/EditorialReviews/EditorialReview/Content");
+                $records{$asin}{editorialreview} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/EditorialReviews/EditorialReview/Content"
+                );
 
-                $records{$asin}{formattedprice} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/OfferSummary/LowestNewPrice/FormattedPrice") || 'N/A';
+                $records{$asin}{formattedprice} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/OfferSummary/LowestNewPrice/FormattedPrice"
+                    )
+                    || 'N/A';
 
-                $records{$asin}{title} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/ItemAttributes/Title");
+                $records{$asin}{title} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/ItemAttributes/Title"
+                );
                 $records{$asin}{author} = join( ", ", @authors );
-                $records{$asin}{pubdate} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/ItemAttributes/PublicationDate");
-                $records{$asin}{reldate} = $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/ItemAttributes/ReleaseDate");
+                $records{$asin}{pubdate} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/ItemAttributes/PublicationDate"
+                );
+                $records{$asin}{reldate} = $xp->findvalue(
+                    "/ItemSearchResponse/Items/Item[$i]/ItemAttributes/ReleaseDate"
+                );
 
             }
         }
 
         $c->stash->{error_msg} = "No Results Found." unless %records;
-        $c->stash->{records} = \%records;
+        $c->stash->{records}   = \%records;
+
         #Capitalize first and last name
         $search_term =~ s/(\w+)/\u\L$1/g;
         $c->stash->{search_term} = $search_term;
@@ -161,13 +189,15 @@ sub query_aws : Private {
     my $keywords = shift;
 
     # Define parts of the REST request.
-    my $baseurl       = "http://webservices.amazon.com/onca/xml";
-    my $service       = "AWSECommerceService";
-    my $accesskey     = "1GNG6V387CH1FWX4H182";
+    my $baseurl   = "http://webservices.amazon.com/onca/xml";
+    my $service   = "AWSECommerceService";
+    my $accesskey = "1GNG6V387CH1FWX4H182";
+
     #my $availability  = "New Release";
     my $operation     = "ItemSearch";
     my $searchindex   = "Books";
     my $responsegroup = "Request,Medium";
+
     #my $version       = "2006-05-17";
 
     # Assemble the REST request URL.
@@ -178,6 +208,7 @@ sub query_aws : Private {
         . "Power=$keywords&"
         . "SearchIndex=$searchindex&"
         . "ResponseGroup=$responsegroup";
+
     #   . "Availability=$operation&"
     #    . "Keywords=$keywords&"
     #    . "Version=$version";
@@ -185,13 +216,12 @@ sub query_aws : Private {
     # Send the request using HTTP GET.
     my $ua = new LWP::UserAgent;
     $ua->timeout(30);
+
     #warn "URL Sent:", $request;
     my $response = get($request);
 
     return $response;
 }
-
-
 
 =head1 AUTHOR
 
