9f016a1b70cc5ce1f2e25a3fbd468ff9a8bf471a
kold <kold@388701f2-3217-0410-a608-c2b1fb36d0f3>
2006-11-23 05:15:43 +0000

    have username validation working
    git-svn-id: svn+ssh://kold.homelinux.com/home/kold/authorwatch@94 388701f2-3217-0410-a608-c2b1fb36d0f3

--- root/static/js/aw_signup.js
+++ root/static/js/aw_signup.js
@@ -2,12 +2,33 @@ var aw_signup = {
    checkUserName : function() {
         $("#signup #username")
         .keyup(function(){
+            if ( this.value.match(/^[A-Za-z0-9_]+$/) && this.value.length > 2 ) {
             $.post('/signup/cu', { username: $(this).val() },
                 function(data) {
-                    //alert(data);
-                    //$("#my_authors").html('');
+                    var jqLabel = $("#signup #username").prev('label');
+                    
+                    //var label = jqLabel[0];
+                    var msg, cn;
+                    if ( data == 1 ) {
+                        msg = 'Username is not available';
+                        cn = 'error';
+                    }
+                    else {
+                        msg = 'Username is available';
+                        cn = 'valid';
+                    }
+
+                    //Append message to label
+                    jqLabel.html('Username: ' + msg);
+                    jqLabel.attr("class",cn);
                 }
             )
+            } //end if
+            else {
+                var jqLabel = $("#signup #username").prev('label');
+                jqLabel.html('Username: Minimum 3 characters and contain A-Z, a-z ,0-9 and underscore (_)');
+                jqLabel.attr("class","error");
+            }
         });
    }
     /*
