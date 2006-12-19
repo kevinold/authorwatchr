7caf80f00c0c1f82d3b4ec8522d7cfb03a603512
kold <kold@388701f2-3217-0410-a608-c2b1fb36d0f3>
2006-12-19 22:07:28 +0000

    updated makefile and added sync script
    git-svn-id: svn+ssh://kold.homelinux.com/home/kold/authorwatch@113 388701f2-3217-0410-a608-c2b1fb36d0f3

---
+++ sync_site.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+#Written by Kevin at Tue 19 Dec 2006 02:41:35 PM EST
+use File::Temp;
+
+my $svnroot = 'svn+ssh://kold@kold.homelinux.com/home/kold/authorwatch';
+my $tmpdir = File::Temp::tempdir('sync_authorwatch.XXXXXX', TMPDIR => 1, CLEANUP => 0);
+
+print "dir is: $tmpdir\n";
+chdir $tmpdir;
+
+print "Exporting authorwatch from SVN...\n";
+
+system qq(svn -q export $svnroot authorwatch);
+
+print "Syncing authorwatch...\n";
+system(qq(rsync -e ssh -zva --delete $tmpdir/authorwatch/ authorwatch\@authorwatch.info:/home/authorwatch/site));
