d93ddefb0d72c867d9b1ed75033a86e75db37f1f
Kevin Old <kold@Macintosh-9.local>
2008-07-25 10:39:11 -0400

    removed unused code and modules moved Net::Amazon token into config switched to using :Path :Args instead of :Private switched to using :Path('rss') :Args(0) instead of :Local made sub results into index for better flow

--- lib/AW/C/Search.pm
+++ lib/AW/C/Search.pm
@@ -3,9 +3,6 @@ package AW::C::Search;
 use strict;
 use warnings;
 use base 'Catalyst::Controller';
-use LWP::Simple qw($ua get);
-use XML::XPath;
-use DateTime;
 use POSIX qw(strftime);
 use URI::Escape;
 use HTML::Scrubber;
@@ -31,33 +28,15 @@ Catalyst Controller.
 
 =cut
 
-=head2 default
+=head2 index - searches for the specified author
 
 =cut
 
-#
-# Output a friendly welcome message
-#
-#sub default : Private {
-#    my ( $self, $c ) = @_;
-
-# Hello World
-#$c->response->body( $c->welcome_message );
-#$c->response->redirect("index.html");
-#}
-
-sub index : Local {
+sub index :Path :Args {
     my ( $self, $c ) = @_;
-
-    $c->forward('results');
+    my $svalue      = $c->request->param("author");
+    my $search_term = $svalue;
     $c->stash->{template} = 'results.tt2';
-
-}
-
-sub results : Private {
-    my ( $self, $c ) = @_;
-    my $svalue      = $c->req->param("author");
-    my $search_term = $c->req->param("author");
 
     #$c->log->debug("**********svalue is: $svalue");
 
@@ -86,24 +65,17 @@ sub results : Private {
             #    Log::Log4perl->init($c->config('log_4_perl_conf'));
             #}
             
-            my $ua = Net::Amazon->new(token => '1GNG6V387CH1FWX4H182', cache => $c->cache);
+            my $ua = Net::Amazon->new(token => $c->config->{na_token}, cache => $c->cache);
             my $response = $ua->search(power => $pw_search, mode => 'books', type => 'Medium', sort => 'daterank');
             # When ready, pass Associate Tag this way
             #my $response = $ua->search(power => $pw_search, mode => "books", type => "Medium", AssociateTag => 'kevin123');
 
-            $c->log->debug("**********RUNNING AMAZON QUERY", Dumper($records));
             if($response->is_success()) {
-                #next if $->year() > 2008;
-                #print Dumper($response->properties);
-                #for my $prop ($response->properties) {
-                #    next if $prop->year() > 2008;
-                #    print $prop->as_string(), " ", $prop->ReleaseDate(), " ", $prop->similar_asins(), "\n";
-                #}
                 $records = $response;
+                #$c->log->debug("**********RUNNING AMAZON QUERY", Dumper($records));
                 #$c->cache->set($authcachekey, $response);
                 #$c->log->debug("**********After cache set", Dumper($c->cache->get($authcachekey)));
             } else {
-                #print "Error: ", $response->message(), "\n";
                 #$c->log->debug("**********Error:", $response->message());
                 $c->stash->{error_msg} = "Error: " . $response->message();
             }
@@ -135,17 +107,14 @@ sub results : Private {
         $search_term =~ s/(\w+)/\u\L$1/g;
         $c->stash->{search_term} = $search_term;
 
-        #if ($c->user) {
-        #    $c->stash->{myauthors} = $c->subreq('/user/myauthors');
-        #}
     }
 
 }
 
-sub rss : Local {
+sub rss :Path('rss') :Args(0) {
     my ($self, $c) = @_;
-    $c->log->debug('in rss b4 index call');
-    $c->forward('results');
+    $c->log->debug('in rss b4 index call', $c->stash->{template});
+    $c->forward('index');
 
     my $feed = XML::Feed->new('RSS');
     $feed->title($c->config->{name} . ' RSS Feed');
@@ -168,211 +137,6 @@ sub rss : Local {
     $c->res->body($feed->as_xml);
 
 }
-
-sub aws : Private {
-    my ( $self, $c ) = @_;
-
-    $c->stash->{template} = 'results.mhtml';
-    my $svalue      = $c->req->param("author");
-    my $search_term = $c->req->param("author");
-
-    #$c->log->debug("**********svalue is: $svalue");
-
-    if ($svalue) {
-
-        #Get search terms urlified
-        my @search = split / /, $svalue;
-        my $keywords = join " ", @search;
-
-        # setup cache key
-        my $authcachekey = join "_", @search;
-
-        my $today = strftime "%m-%Y", localtime;
-            # Real search
-            #"author: $keywords and pubdate: after $today and binding: Hardcover"
-        my $pw_search = uri_escape(
-            "author: $keywords and binding: Hardcover"
-        );
-    
-        
-        my $records;
-        
-        unless ( $records = $c->cache->get($authcachekey) ) {
-
-        # Perform search
-        my $response = query_aws($pw_search);
-
-        #$c->log->debug("Response: $response");
-
-        # Process XML response with XPath.
-        my $xp = XML::XPath->new( xml => $response );
-
-        if ( $xp->find("//Error") ) {
-            my $ec = $xp->findvalue("//Error/Code");
-            my $em = $xp->findvalue("//Error/Message");
-
-            #$c->stash->{error_msg} = "$ec: $em" ;
-            $c->stash->{error_msg} = "No Results Found.";
-
-            #Put "Author Suggest" code here
-        }
-        else {
-            for ( my $i = 1; $i <= 10; $i++ ) {
-                if ( !$xp->find("/ItemSearchResponse/Items/Item[$i]") ) {
-                    last;
-                }
-
-                # Remove Large Print Editions
-                next if $xp->findvalue("/ItemSearchResponse/Items/Item[$i]/ItemAttributes/Edition") =~ /large/i;
-
-                my @authors;
-                for (
-                    my $j = 1;
-                    $j <= $xp->findvalue(
-                        "count(/ItemSearchResponse/Items/Item[$i]/ItemAttributes/Author)"
-                    );
-                    $j++
-                    )
-                {
-                    push @authors,
-                        $xp->findvalue(
-                        "/ItemSearchResponse/Items/Item[$i]/ItemAttributes/Author[$j]"
-                        );
-                }
-
-#Check pubdate and make sure year is a valid year and is not too far in the future
-                my $pd = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/ItemAttributes/PublicationDate"
-                );
-                my ( $yr, $mon, $day ) = split /-/, $pd;
-                warn "yr is: ", $yr;
-                next unless $yr  =~ /^200/;
-                next unless $mon =~ /^[0|1][0-9]/;
-                next unless $day =~ /^[0|1|2|3][0-9]/;
-
-
-                my $asin = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/ASIN");
-
-                $records->{$asin}{detailpageurl} = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/DetailPageURL");
-
-                $records->{$asin}{smallimage} = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/SmallImage/URL");
-                $records->{$asin}{smallimageheight} = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/SmallImage/Height");
-                $records->{$asin}{smallimagewidth} = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/SmallImage/Width");
-
-                $records->{$asin}{mediumimage} = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/MediumImage/URL");
-                $records->{$asin}{mediumimageheight} = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/MediumImage/Height");
-                $records->{$asin}{mediumimagewidth} = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/MediumImage/Width");
-
-                $records->{$asin}{largeimage} = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/LargeImage/URL");
-                $records->{$asin}{largeimageheight} = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/LargeImage/Height");
-                $records->{$asin}{largeimagewidth} = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/LargeImage/Width");
-
-                my $editorialreview = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/EditorialReviews/EditorialReview/Content"
-                );
-                
-                # Clean HTML from Editorial Review
-                $records->{$asin}{editorialreview} = straighten_html($editorialreview);
-
-                $records->{$asin}{formattedprice} = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/Offers/Offer/OfferListing/Price/FormattedPrice"
-                    )
-                    || 'N/A';
-
-                $records->{$asin}{title} = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/ItemAttributes/Title"
-                );
-                $records->{$asin}{author} = join( ", ", @authors );
-                $records->{$asin}{pubdate} = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/ItemAttributes/PublicationDate"
-                );
-                $records->{$asin}{reldate} = $xp->findvalue(
-                    "/ItemSearchResponse/Items/Item[$i]/ItemAttributes/ReleaseDate"
-                );
-
-            }
-        }
-
-            $c->cache->set($authcachekey, $records);
-        }
-        $c->stash->{records}   = $records;
-        $c->stash->{error_msg} = "No Results Found." unless $records;
-
-        #Capitalize first and last name
-        $search_term =~ s/(\w+)/\u\L$1/g;
-        $c->stash->{search_term} = $search_term;
-        
-    }
-    else {
-        $c->stash->{error_msg} = "No search parameters entered.";
-    }
-
-}
-
-=head2 end
-
-Forward to Mason View
-
-=cut
-
-#sub end : Private {
-#    my ( $self, $c ) = @_;
-
-    # Forward to View unless response body is already defined
-#    $c->forward( $c->view('Mason') ) unless $c->response->body;
-#}
-
-sub query_aws : Private {
-    my $keywords = shift;
-
-    # Define parts of the REST request.
-    my $baseurl   = "http://webservices.amazon.com/onca/xml";
-    my $service   = "AWSECommerceService";
-    my $accesskey = "1GNG6V387CH1FWX4H182";
-
-    #my $availability  = "New Release";
-    my $operation     = "ItemSearch";
-    my $searchindex   = "Books";
-    my $responsegroup = "Request,Large";
-
-    #my $version       = "2006-05-17";
-
-    # Assemble the REST request URL.
-    my $request = "$baseurl?"
-        . "Service=$service&"
-        . "AWSAccessKeyId=$accesskey&"
-        . "Operation=$operation&"
-        . "Power=$keywords&"
-        . "SearchIndex=$searchindex&"
-        . "ResponseGroup=$responsegroup";
-
-    #   . "Availability=$operation&"
-    #    . "Keywords=$keywords&"
-    #    . "Version=$version";
-
-    # Send the request using HTTP GET.
-    my $ua = new LWP::UserAgent;
-    $ua->timeout(30);
-
-    warn "URL Sent:", $request;
-    my $response = get($request);
-
-    return $response;
-}
-
-
-
 
 sub straighten_html {
     my $body_content = shift;
