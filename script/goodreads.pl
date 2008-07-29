459fff2f67c8b531c12ffcb68a2a82691a80217a
Kevin Old <kevin@kevinold.com>
2008-07-29 17:36:06 -0400

    placeholder script for goodreads.com has api key for future use to load authors from books on a users shelf

---
+++ script/goodreads.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+#Written by Kevin at Tue Jul 29 17:32:56 2008
+#
+
+#Goodreads.com API key (assigned to param 'key'):
+# ODA=-0c82f299532f9d7a93ba17b12de9148f1757a057
